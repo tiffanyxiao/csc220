b9cd3d292c93a6abdb0c9060f2874291ff32a681
Karen Santamaria <sanmakaren@gmail.com>
2018-02-13 23:33:23 -0500

    .

--- p2/README.txt
+++ p2/README.txt
@@ -1,1 +1,1 @@
-personal copy of project 2 (for organizational purposes)
+stuff
--- p2/gene.py
+++ p2/gene.py
@@ -7,7 +7,7 @@ Given two strings representing snippets of genes (letters ACGT), identify the
 shortest string that could contain them both as subsequences.
 '''
 
-def print_in_format(string1, string2, union_result):
+def print_union(string1, string2, union_result):
     '''print out the result in a pretty format'''
     print("**************************************")
     print("String 1 -", string1)
@@ -16,7 +16,7 @@ def print_in_format(string1, string2, union_result):
     print(union_result, "(" + str(len(union_result)) + ")")
     print("**************************************")
 
-def make_union_string(string1, string2,substring):
+def string_union(string1, string2,substring):
     '''make the shortest string  that contains string1 and string2 as subsequences'''
     union_result = ""
     if string1.endswith(substring):
@@ -25,7 +25,7 @@ def make_union_string(string1, string2,substring):
         union_result = string2+ string1.replace(substring,"")
     return union_result
 
-def find_overlap(string1, string2, len_string1, len_string2):
+def get_overlap(string1, string2, len_string1, len_string2):
     '''find the substring that appear in both strings'''
 
     #string1 is associated with horizontal diraction
@@ -38,23 +38,24 @@ def find_overlap(string1, string2, len_string1, len_string2):
         for row in range (0,len_string2):
             if (string1[col] == string2[row]):
                 matrix[row][col] = 1
+
     #for every cell in the matrix, add the value of the upper-reight cell to the current cell
     #so the value of the cells will be the number of cosecutive match if there is any
-    largest_count = 0
-    index_of_largest = []
+    overlap_len = 0
+    overlap_position = []
     for col in range (0,len_string1):
         for row in range (0,len_string2):
             if matrix[row][col] == 1 and row!=0 and col!=0:
                 matrix[row][col] = matrix[row][col] + matrix[row-1][col-1]
-                if matrix[row][col]>largest_count:
-                    largest_count = matrix[row][col]
-                    index_of_largest = [row,col]
+                if matrix[row][col]>overlap_len:
+                    overlap_len = matrix[row][col]
+                    overlap_position = [row,col]
 
     #construct the substring
-    if (len(index_of_largest)>0):
-        overlap = string1[index_of_largest[1]]
-        for l in range(1, largest_count):
-            overlap = string1[index_of_largest[1]-l]+overlap
+    if (overlap_position):
+        overlap = string1[overlap_position[1]]
+        for l in range(1, overlap_len):
+            overlap = string1[overlap_position[1]-l]+overlap
 
     else:
         overlap = ""
@@ -64,20 +65,21 @@ def find_overlap(string1, string2, len_string1, len_string2):
 
 def main():
     gene_lst = ["A","C","T","G"]
+    #test cases
+    # string1 = "CG"
+    # string2 = "AA"
 
-    string1 = input("The first gene" + "\n")
-    string2 = input("The second gene" + "\n")
+    string1 = input("Input the first sequence" + "\n")
+        for l in string1:
+            if (l not in gene_lst):
+                raise ValueError('String1 is not a gene')
 
-    for l in string1:
-        if (l not in gene_lst):
-            raise ValueError('String1 is not a gene')
-
-
+    string2 = input("Input the second sequence" + "\n")
     for l in string2:
         if (l not in gene_lst):
             raise ValueError('String2 is not a gene')
 
-    substring = find_overlap(string1,string2, len(string1), len(string2))
-    print_in_format(string1,string2,make_union_string(string1,string2, substring))
+    substring = get_overlap(string1,string2, len(string1), len(string2))
+    print_union(string1,string2,string_union(string1,string2, substring))
 
 main()
