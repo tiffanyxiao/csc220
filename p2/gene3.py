301ee125284a8c9f1557df2dfceb1d67685c10a1
Karen Santamaria <sanmakaren@gmail.com>
2018-02-13 22:48:14 -0500

    good need more tests

--- p2/gene3.py
+++ p2/gene3.py
@@ -1,47 +1,37 @@
 '''
-Objective: given two strings representing snippets of genes (letters ACGT),
-identify the shortest string that could contain them both as subsequences.
+Authors: Heidi Tsang, Tiffany Xiao, Karen Santamaria
+Date: February  15, 2018
+Title: Gene Splicing
 
-
-********************************************
-string 1 – AACCTGT     string 2 – CTGTACG
-shortest string that has both as substring
-AACCTGTACG (length 10)
-********************************************
-
-The submission:
-
-successfully reads in a pair of strings
-outputs a string that contains both input strings as a subsequence
-checks to ensure input is valid before beginning calculation; if not, throws an exception
-Second level (2 points)
-
-The submission:
-
-correctly reports the length of the shortest string that contains both input strings
-as a subsequence (but possibly fails to return the string itself)
-is at least as efficient as the recursive solution i.e. no worse than O(2^n)
-Third level (2 points)
-
-The submission:
-
-is more efficient than the recursive solution
-correctly returns the shortest string that contains both input strings as a subsequence
-*Note* to get efficiency-related points, you must include a brief discussion of the program's efficiency in your README
+Given two strings representing snippets of genes (letters ACGT), identify the
+shortest string that could contain them both as subsequences.
 '''
-#import numpy as np
-def make_shortest_string(string1, string2,substring):
-    shortest_string = ""
+
+def print_in_format(string1, string2, union_result):
+    '''print out the result in a pretty format'''
+    print("**************************************")
+    print("String 1 -", string1)
+    print("String 2 -", string2)
+    print("shortest string that has both as substring")
+    print(union_result, "(" + str(len(union_result)) + ")")
+    print("**************************************")
+
+def make_union_string(string1, string2,substring):
+    '''make the shortest string  that contains string1 and string2 as subsequences'''
+    union_result = ""
     if string1.endswith(substring):
-        shortest_string = string1+ string2.replace(substring,"")
+        union_result = string1+ string2.replace(substring,"")
     else:
-        shortest_string = string2+ string1.replace(substring,"")
-    return shortest_string
-def intersection(string1, string2, len_string1, len_string2):
-    #fill in everything with zeros
-    matrix = [[0 for col in range(len_string1)] for row in range(len_string2)]
+        union_result = string2+ string1.replace(substring,"")
+    return union_result
+
+def find_overlap(string1, string2, len_string1, len_string2):
+    '''find the substring that appear in both strings'''
+
     #string1 is associated with horizontal diraction
     #string2 is associated with vertical direction
+    #everything initially filled in with 0's
+    matrix = [[0 for col in range(len_string1)] for row in range(len_string2)]
 
     #fill in 1's with matches
     for col in range (0,len_string1):
@@ -61,18 +51,33 @@ def intersection(string1, string2, len_string1, len_string2):
                     index_of_largest = [row,col]
 
     #construct the substring
-    overlap = string1[index_of_largest[1]]
-    for l in range(1, largest_count):
-        overlap = string1[index_of_largest[1]-l]+overlap
+    if (len(index_of_largest)>0):
+        overlap = string1[index_of_largest[1]]
+        for l in range(1, largest_count):
+            overlap = string1[index_of_largest[1]-l]+overlap
+
+    else:
+        overlap = ""
     return overlap
 
 
 
 def main():
-    str_lst = ["A","C","T","G"]
-    string1 = "AACCTGT"
-    string2 = "CTGTACG"
-    substring = intersection(string1,string2, len(string1), len(string2))
-    short = make_shortest_string(string1,string2, substring)
-    print(short)
+    gene_lst = ["A","C","T","G"]
+
+    string1 = input("The first gene" + "\n")
+    string2 = input("The second gene" + "\n")
+
+    for l in string1:
+        if (l not in gene_lst):
+            raise ValueError('String1 is not a gene')
+
+
+    for l in string2:
+        if (l not in gene_lst):
+            raise ValueError('String2 is not a gene')
+
+    substring = find_overlap(string1,string2, len(string1), len(string2))
+    print_in_format(string1,string2,make_union_string(string1,string2, substring))
+
 main()
