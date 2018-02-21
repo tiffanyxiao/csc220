55df65b6f9feb48567828e263043399189fa2c6b
Karen Santamaria <sanmakaren@gmail.com>
2018-02-21 13:57:32 -0500

    .

--- p3/p3.py
+++ p3/p3.py
@@ -30,7 +30,7 @@ def pantrySums(boxes, n_items, total):
 
     #want to include 0 in the table so we need total+1
     #[i][j] where i indicates row and j indicates column
-    arrayValues = [["U" for col in range(total+1)] for row in range(len(pantry_vals))]
+    arrayValues = [["---" for col in range(total+1)] for row in range(len(pantry_vals))]
 
 
     #fill all the 0's with true because we know it's true
@@ -46,59 +46,51 @@ def pantrySums(boxes, n_items, total):
 
     #now begin systematically filling in the array:
     for row in range (1,len(pantry_vals)):
-        for col in range (1,total+1):
+        for col in range (1, total+1):
+            if (pantry_vals[row] > col ):
+                arrayValues[row][col] = arrayValues[row-1][col]
 
-            if (col < pantry_vals[row]):
-                if (arrayValues[row-1][col] == True):
-                    arrayValues[row][col] = True
+            elif (pantry_vals[row] < col):
+                if (arrayValues[row-1][col]):
+                    arrayValues[row][col] = arrayValues[row-1][col]
                 else:
-                    arrayValues[row][col] = False
+                    arrayValues[row][col] = arrayValues[row-1][col-pantry_vals[row]]
 
-            # elif (col == pantry_vals[row]):
-            #     arrayValues[row][col] = True
-
-            elif (col >= pantry_vals[row]):
-                #go up and back to the value to find it
-
-                if (arrayValues[row-1][col-pantry_vals[row]]== True):
-                    arrayValues[row][col] = True
-                else:
-                    arrayValues[row][col] = arrayValues[row-1][col-row]
-
-    # for i in arrayValues:
-    #     print(i[90:])
-
-    pointer = [len(pantry_vals)-1, total]
+            elif (pantry_vals[row] == col):
+                arrayValues[row][col] = True
 
 
-    #now retracing to get a subset
-    if (arrayValues[len(pantry_vals)-1][total]):
-        pointer = [len(pantry_vals)-1, total]
 
-        while(pointer[1] >= 0 and pointer[0] >= 0):
-
-            kp = pointer[0]-1
-            print(pantry_vals[kp], kp)
-
-            print(pointer)
-            while (arrayValues[pointer[0]][pointer[1]] == True):
-                pointer=[pointer[0]-1, pointer[1]]
-            pointer = [pointer[0],pointer[1]-pantry_vals[kp]]
+    # pointer = [len(pantry_vals)-1, total]
+    #
+    #
+    # #now retracing to get a subset
+    # if (arrayValues[len(pantry_vals)-1][total]):
+    #     pointer = [len(pantry_vals)-1, total]
+    #
+    #     while(pointer[1] >= 0 and pointer[0] >= 0):
+    #
+    #         kp = pointer[0]-1
+    #         print(pantry_vals[kp], kp)
+    #
+    #         print(pointer)
+    #         while (arrayValues[pointer[0]][pointer[1]] == True):
+    #             pointer=[pointer[0]-1, pointer[1]]
+    #         pointer = [pointer[0],pointer[1]-pantry_vals[kp]]
 
 
 
 
-    for i in arrayValues:
-        print(i)
 
 
-    return result
+    return arrayValues[len(pantry_vals)-1][total]
+    #return result
 
 def main():
-    boxes = { "chips":2, "detergent":3, "cereal":7,"pepsi":8, "chaps":10}
-    #boxes = {"pepsi":55, "chips":25, "detergent":30, "cereal":15}
+    #boxes = { "chips":2, "detergent":3, "cereal":7,"pepsi":8, "chaps":10}
+    boxes = {"pepsi":55, "chips":25, "detergent":30, "cereal":15}
     num_boxes = len(boxes)
-    total = 11
+    total = 100
     fullbox = pantrySums(boxes, num_boxes, total)
     print(fullbox, "fullbox")
 
