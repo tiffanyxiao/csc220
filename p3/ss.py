c26a9f6a78d4233a9d98099bd1258b720c1f9d7f
Karen Santamaria <sanmakaren@gmail.com>
2018-02-20 20:45:21 -0500

    .

--- p3/ss.py
+++ p3/ss.py
@@ -1,6 +1,8 @@
 '''problem is that it doesnt find all the possible combinations given that it
 increases in increment down the table. solution COULD BE THAT WE REDO THE FUNCTION FOR ALL
-COMBINATIONS if they add up to some number greater than the given total'''
+COMBINATIONS if they add up to some number greater than the given total
+
+multiprocessing might be a good idea for working with multiple tables at a time?'''
 
 
 
